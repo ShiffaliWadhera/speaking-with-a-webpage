e39571aa30b08612252693a24d02a3ded9775f48
ShiffaliWadhera <41578324+ShiffaliWadhera@users.noreply.github.com>
2021-02-19 20:19:56 +0530

    Update TranscribeSocket.java

--- 04-speech/src/main/java/com/example/flexible/speak/TranscribeSocket.java
+++ 04-speech/src/main/java/com/example/flexible/speak/TranscribeSocket.java
@@ -159,7 +159,6 @@ public class TranscribeSocket extends WebSocketAdapter
       logger.info("Got result :" + result);
       String transcript = result.getAlternatives(0).getTranscript();
       logger.info("Transcript : " + transcript);
-      String req = "{\"query_input\": {\"text\": {\"text\": \""+transcript+"\",\"language_code\": \"en-US\"}}}";
       logger.info("Get env token : " + System.getenv("token"));
       logger.info("Get property token : " + System.getProperty("token"));
       HttpClient client = new DefaultHttpClient();
@@ -178,7 +177,7 @@ public class TranscribeSocket extends WebSocketAdapter
       logger.info("Token argument " + token);
       post.addHeader("Authorization", "Bearer " + token);
       try {
-        StringEntity entity = new StringEntity(req);
+        StringEntity entity = new StringEntity(transcript);
         post.setEntity(entity);
 
         HttpResponse res = client.execute(post);
