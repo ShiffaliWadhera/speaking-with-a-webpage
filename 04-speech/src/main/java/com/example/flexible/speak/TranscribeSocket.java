a8a723b51c1c2f18a6dbd6df940b1d302751af07
ShiffaliWadhera <41578324+ShiffaliWadhera@users.noreply.github.com>
2021-02-19 20:39:12 +0530

    Update TranscribeSocket.java

--- 04-speech/src/main/java/com/example/flexible/speak/TranscribeSocket.java
+++ 04-speech/src/main/java/com/example/flexible/speak/TranscribeSocket.java
@@ -159,7 +159,7 @@ public class TranscribeSocket extends WebSocketAdapter
       logger.info("Got result :" + result);
       String transcript = result.getAlternatives(0).getTranscript();
       logger.info("Transcript : " + transcript);
-      String req = "{\"query_input\":{\"text\":{\"text\": \""+transcript+"\",\"language_code\": \"en-US\"}}}";
+      String req = "{\"query_input\":{\"text\":{\"text\":\""+transcript+"\",\"language_code\":\"en-US\"}}}";
       logger.info("Get env token : " + System.getenv("token"));
       logger.info("Get property token : " + System.getProperty("token"));
       HttpClient client = new DefaultHttpClient();
