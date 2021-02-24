81a766601181848a0ba28a5371bac1daad8505ee
ShiffaliWadhera <41578324+ShiffaliWadhera@users.noreply.github.com>
2021-02-24 11:46:57 +0530

    Update main.js

--- 04-speech/src/main/webapp/javascript/main.js
+++ 04-speech/src/main/webapp/javascript/main.js
@@ -256,9 +256,9 @@
     function onTranscription(e) {
       var result = JSON.parse(e.data);
       alert(result);
-      if (result.fulfillmentText\) {
+      if (result.fulfillmentText) {
         alert("going maddddd....");
-        transcript.current.innerHTML =  result.fulfillmentText\;
+        transcript.current.innerHTML =  result.fulfillmentText;
         transcript.current = document.createElement('div');
         transcript.el.appendChild(transcript.current);
         
